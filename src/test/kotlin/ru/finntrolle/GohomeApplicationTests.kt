fc78c925333ad34332b9dad35a9b2f1c8a6536af
maxim.syachin <MSyachin@luxoft.com>
2016-11-15 00:26:33 +0300

    Fixing tests

--- src/test/kotlin/ru/finntrolle/GohomeApplicationTests.kt
+++ src/test/kotlin/ru/finntrolle/GohomeApplicationTests.kt
@@ -3,14 +3,16 @@ package ru.finntrolle
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.SpringRunner
 
+@TestPropertySource(locations = arrayOf("classpath:repositories-test.properties"))
 @RunWith(SpringRunner::class)
 @SpringBootTest
 class GohomeApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
